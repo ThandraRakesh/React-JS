2169954b1ef8f333bbff9140ce30fef45f07ff54
RakeshThandra <RakeshThandra@SNP-HYD-LPT-095>
2026-01-29 07:54:53 -0500

    episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement( "div", {id:"child"},[
+          React.createElement ("h1", {}, "Im  from H1 Tag"),
+          React.createElement ("h2", {}, "Im  from H2 Tag"),
+        ]),
+         React.createElement( "div", {id:"child2"},[
+          React.createElement ("h1", {}, "Im  from H1 Tag"),
+          React.createElement ("h2", {}, "Im  from H2 Tag"),
+        ]),
+)
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
